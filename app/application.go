64234f18f7d9621f1e07d53fa041115f0b3a3a04
Adam Vu <gvu0110@gmail.com>
2021-05-22 09:55:53 -0400

    Added application timeout

--- app/application.go
+++ app/application.go
@@ -2,6 +2,7 @@ package app
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 )
@@ -16,6 +17,10 @@ func StartApplication() {
 	srv := &http.Server{
 		Handler: router,
 		Addr:    "localhost:8082",
+		// Good practice: enforce timeouts for servers you create!
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+		IdleTimeout:  60 * time.Second,
 	}
 
 	if err := srv.ListenAndServe(); err != nil {

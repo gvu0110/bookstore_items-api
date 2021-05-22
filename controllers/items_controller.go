bd489bea2291b027ae53ed7feb0241860546a696
Adam Vu <gvu0110@gmail.com>
2021-05-22 09:56:26 -0400

    Added item create controller

--- controllers/items_controller.go
+++ controllers/items_controller.go
@@ -1,6 +1,13 @@
 package controllers
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/gvu0110/bookstore_items-api/domain/items"
+	"github.com/gvu0110/bookstore_items-api/services"
+	"github.com/gvu0110/bookstore_items-api/utils/http_utils"
+	"github.com/gvu0110/bookstore_oauth-go/oauth"
+)
 
 var (
 	ItemsController itemsControllerInterface = &itemsController{}
@@ -16,6 +23,23 @@ type itemsController struct {
 }
 
 func (c *itemsController) Create(w http.ResponseWriter, r *http.Request) {
+	if err := oauth.AuthenticateRequest(r); err != nil {
+		http_utils.ResponseRESTError(w, *err)
+		return
+	}
+
+	// TODO: Unmarshall request into the item struct
+	item := items.Item{
+		Seller: oauth.GetCallerID(r),
+	}
+
+	result, err := services.ItemsService.Create(item)
+	if err != nil {
+		http_utils.ResponseRESTError(w, *err)
+		return
+	}
+
+	http_utils.ResponseJSON(w, http.StatusCreated, result)
 }
 
 func (c *itemsController) Get(w http.ResponseWriter, r *http.Request) {

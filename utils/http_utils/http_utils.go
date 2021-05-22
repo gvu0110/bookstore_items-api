dfb9ae5c9967b8aa6cc20efdead4f2eea2f1a159
Adam Vu <gvu0110@gmail.com>
2021-05-22 09:57:17 -0400

    Added public rest error response

--- utils/http_utils/http_utils.go
+++ utils/http_utils/http_utils.go
@@ -3,10 +3,16 @@ package http_utils
 import (
 	"encoding/json"
 	"net/http"
+
+	"github.com/gvu0110/bookstore_utils-go/rest_errors"
 )
 
 func ResponseJSON(w http.ResponseWriter, statusCode int, body interface{}) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(statusCode)
 	json.NewEncoder(w).Encode(body)
+}
+
+func ResponseRESTError(w http.ResponseWriter, err rest_errors.RESTError) {
+	ResponseJSON(w, err.StatusCode, err)
 }

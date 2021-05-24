f8e47a7e1ac64b52b84247f99cff3eb1dca4cdbb
Adam Vu <gvu0110@gmail.com>
2021-05-23 23:21:39 -0400

    Added logger and implemented index function

--- clients/elasticsearch/es_client.go
+++ clients/elasticsearch/es_client.go
@@ -2,8 +2,10 @@ package elasticsearch
 
 import (
 	"context"
+	"fmt"
 	"time"
 
+	"github.com/gvu0110/bookstore_items-api/logger"
 	"github.com/olivere/elastic"
 )
 
@@ -13,7 +15,7 @@ var (
 
 type esClientInterface interface {
 	setClient(client *elastic.Client)
-	Index(interface{}) (*elastic.IndexResponse, error)
+	Index(string, interface{}) (*elastic.IndexResponse, error)
 }
 
 type esClient struct {
@@ -26,7 +28,7 @@ func (c *esClient) setClient(client *elastic.Client) {
 
 func Init() {
 	client, err := elastic.NewClient(
-		elastic.SetURL("localhost:9200"),
+		elastic.SetURL("http://localhost:9200"),
 		elastic.SetHealthcheckInterval(10*time.Second),
 		elastic.SetSniff(false),
 	)
@@ -36,7 +38,12 @@ func Init() {
 	Client.setClient(client)
 }
 
-func (c *esClient) Index(interface{}) (*elastic.IndexResponse, error) {
+func (c *esClient) Index(index string, doc interface{}) (*elastic.IndexResponse, error) {
 	ctx := context.Background()
-	return c.client.Index().Do(ctx)
+	result, err := c.client.Index().Index(index).BodyJson(doc).Do(ctx)
+	if err != nil {
+		logger.Error(fmt.Sprintf("Error when trying to index document in index %s", index), err)
+		return nil, err
+	}
+	return result, nil
 }
